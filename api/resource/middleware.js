0d24e0e27569e686d259fcbbd6f561d3ac65c6f6
Rick <ricksmycodeguy@gmail.com>
2021-10-08 14:44:26 -0500

    add extra validation to validateResources MW

--- api/resource/middleware.js
+++ api/resource/middleware.js
@@ -3,7 +3,8 @@ const validateResource = (req, res, next) => {
     if (
         resource_name === undefined ||
         typeof resource_name !== 'string' ||
-        !resource_name.trim()
+        !resource_name.trim() ||
+        resource_name === ''
     ) {
         next({
             status: 400,
