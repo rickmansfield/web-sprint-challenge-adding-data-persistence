868fc7c78f22e0d69a48804ab66e8d0105550122
Rick <ricksmycodeguy@gmail.com>
2021-10-08 15:21:35 -0500

    Wire Tasks [POST] to MW and Model

--- api/task/model.js
+++ api/task/model.js
@@ -11,4 +11,17 @@ async function fetchAllTasks() {
         }
     })
 }
-module.exports = { fetchAllTasks }
+async function findTaskById(id) {
+    const row = await db('tasks as t')
+        .join('projects as p', 'p.project_id', 't.project_id')
+        .select('t.*', 'p.project_name', 'p.project_description')
+        .where('task_id', id)
+        .first()
+    return { ...row, task_completed: row.task_completed ? true : false }
+}
+
+async function insert(task) {
+    const [id] = await db('tasks').insert(task)
+    return findTaskById(id)
+}
+module.exports = { fetchAllTasks, findTaskById, insert }
