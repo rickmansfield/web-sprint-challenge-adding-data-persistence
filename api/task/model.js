e427b8b87fbb5a41ce01acd7c78b3dfeaf8ed031
Rick <ricksmycodeguy@gmail.com>
2021-10-08 15:09:09 -0500

    Add Tasks Router, Mw, model. [GET} working 5x5

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,14 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig')
+
+async function fetchAllTasks() {
+    const rows = await db('tasks as t')
+        .join('projects as p', 'p.project_id', 't.project_id')
+        .select('t.*', 'p.project_name', 'p.project_description')
+    return rows.map((item) => {
+        return {
+            ...item,
+            task_completed: item.task_completed ? true : false,
+        }
+    })
+}
+module.exports = { fetchAllTasks }
